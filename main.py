5ed9884fad56465fbbbad4e9a1ad743fdb179dec
LordSkull <brunoformisano0@gmail.com>
2026-05-07 21:21:17 +0200

    edited main.py

--- main.py
+++ main.py
@@ -10,3 +10,24 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Caricamento del dataset
 df = pd.read_csv('PhishingData.csv')
+
+
+X = df.drop('label', axis=1)   
+y = df['label']   
+
+# Suddivisione del dataset in training set e test set
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=42)
+
+# Creazione del modello Random Forest Classifier
+model = RandomForestClassifier(n_estimators=50, random_state=42)
+
+df.describe()
+
+# Addestramento del modello
+model.fit(X_train, y_train)
+
+# Valutazione del modello
+x_test_pred = model.predict(X_test)
+accuracy = np.mean(x_test_pred == y_test)
+print(f'Accuracy: {accuracy:.2f}')
+
